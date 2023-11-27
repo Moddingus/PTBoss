686d14b87bde684a10888216ccf41a909f3cb54e
Max <Max@siebengartner.com>
2023-11-27 21:55:26 +0800

    fixed expert mode bug with consumed buff

--- CatBoss/Consumed.cs
+++ CatBoss/Consumed.cs
@@ -34,7 +34,6 @@ namespace TopHatCatBoss.CatBoss
             Main.debuff[Type] = true;  // Is it a debuff?
             Main.pvpBuff[Type] = true; // Players can give other players buffs, which are listed as pvpBuff
             Main.buffNoSave[Type] = true; // Causes this buff not to persist when exiting and rejoining the world
-            BuffID.Sets.LongerExpertDebuff[Type] = true; // If this buff is a debuff, setting this to true will make this buff last twice as long on players in expert mode
         }
         public override bool PreDraw(SpriteBatch spriteBatch, int buffIndex, ref BuffDrawParams drawParams)
         {
